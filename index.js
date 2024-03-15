7afe28003626359f1301dd9281f96671c1d2c1df
rahul mahato <rahulmahato0852@gmail.com>
2024-03-15 18:29:23 +0530

    cookie aadded

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static("post"))
 app.use(express.json())
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://banaonodejssubmission.onrender.com",
         credentials: true
     }
 ))
